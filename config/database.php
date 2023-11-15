9044e93f80c23569b469e73fd945b525be815b06
yasirrose <yasirzinbox@gmail.com>
2023-11-15 19:22:38 +0500

    Update database configuration

--- config/database.php
+++ config/database.php
@@ -2,8 +2,8 @@
 
 // define database connection
   define('DB_SERVER', 'localhost'); // eg, localhost - should not be empty for productive servers
-  define('DB_SERVER_USERNAME', 'root');
-  define('DB_SERVER_PASSWORD', '');
-  define('DB_SERVER_PORT', '');		
-  define('DB_DATABASE', 'veloujsf_pm');  	  
+  define('DB_SERVER_USERNAME', 'veloujsf_pbm');
+  define('DB_SERVER_PASSWORD', 'i(49Slp@@EQ7W[)D');
+  define('DB_SERVER_PORT', '');
+  define('DB_DATABASE', 'veloujsf_pbm');
   
