3c528e7c13c66e97f6232924d08c5eeca95beb0e
yasirrose <yasirzinbox@gmail.com>
2023-11-15 19:28:02 +0500

    Update database password

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 // define database connection
   define('DB_SERVER', 'localhost'); // eg, localhost - should not be empty for productive servers
   define('DB_SERVER_USERNAME', 'veloujsf_pbm');
-  define('DB_SERVER_PASSWORD', 'i(49Slp@@EQ7W[)D');
+  define('DB_SERVER_PASSWORD', 'i(49Slp@@EQ7W[)P');
   define('DB_SERVER_PORT', '');
   define('DB_DATABASE', 'veloujsf_pbm');
   
